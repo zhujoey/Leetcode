e9882d84a5c44cb4b1286d1b4e078cfc27e6b68c
Joey <zhujoey@outlook.com>
2024-12-27 13:37:03 -0800

    changed problem 17
    made it much faster by using push back

--- 1-100/17Lettercombinationsofaphonenumber/Lettercombinationsofaphonenumber.cpp
+++ 1-100/17Lettercombinationsofaphonenumber/Lettercombinationsofaphonenumber.cpp
@@ -10,161 +10,164 @@ public:
         {
             return {};
         }
-        return recursive(digits, 0);
+        return makeLetterCombinationsRecursively(digits, digits.size() - 1);
     }
-    std::vector<std::string> recursive(std::string digits, int index)
+
+    // Complexity: O(n ^ 2)
+    std::vector<std::string> makeLetterCombinationsRecursively(std::string digits, int index)
     {
-        if (index == digits.size())
+        if (index == -1)
         {
-                return {""};
+            return {""};
         }
 
-        std::vector<std::string> ans = {};
-        std::vector<std::string> temp = recursive(digits, index + 1);
+        std::vector<std::string> result = {};
+        std::vector<std::string> temp = makeLetterCombinationsRecursively(digits, index - 1);
+        int times = 0;
 
         if(digits[index] == '7' || digits[index] == '9')
         {
-            ans.insert(ans.end(), temp.begin(), temp.end());
-            ans.insert(ans.end(), temp.begin(), temp.end());
-            ans.insert(ans.end(), temp.begin(), temp.end());
-            ans.insert(ans.end(), temp.begin(), temp.end());
+            times = 4;
         }
         else
         {
-            ans.insert(ans.end(), temp.begin(), temp.end());
-            ans.insert(ans.end(), temp.begin(), temp.end());
-            ans.insert(ans.end(), temp.begin(), temp.end());
-        }     
+            times = 3;
+        }
+
+        for (int i = 0; i < times; ++i)
+        {
+            result.insert(result.end(), temp.begin(), temp.end());
+        }
         
         if (digits[index] == '2')
         {
-            for (int i = 0; i < ans.size() / 3; ++i)
+            for (int i = 0; i < result.size() / 3; ++i)
             {
-                ans[i] = "a" + ans[i];
+                result[i].push_back('a');
             }
-            for (int i = ans.size() / 3; i < ans.size() / 3 * 2; ++i)
+            for (int i = result.size() / 3; i < result.size() / 3 * 2; ++i)
             {
-                ans[i] = "b" + ans[i];
+                result[i].push_back('b');
             }
-            for (int i = ans.size() / 3 * 2; i < ans.size(); ++i)
+            for (int i = result.size() / 3 * 2; i < result.size(); ++i)
             {
-                ans[i] = "c" + ans[i];
+                result[i].push_back('c');
             }
         }
         else if (digits[index] == '3')
         {
-            for (int i = 0; i < ans.size() / 3; ++i)
+            for (int i = 0; i < result.size() / 3; ++i)
             {
-                ans[i] = "d" + ans[i];
+                result[i].push_back('d');
             }
-            for (int i = ans.size() / 3; i < ans.size() / 3 * 2; ++i)
+            for (int i = result.size() / 3; i < result.size() / 3 * 2; ++i)
             {
-                ans[i] = "e" + ans[i];
+                result[i].push_back('e');
             }
-            for (int i = ans.size() / 3 * 2; i < ans.size(); ++i)
+            for (int i = result.size() / 3 * 2; i < result.size(); ++i)
             {
-                ans[i] = "f" + ans[i];
+                result[i].push_back('f');
             }
         }
         else if (digits[index] == '4')
         {
-            for (int i = 0; i < ans.size() / 3; ++i)
+            for (int i = 0; i < result.size() / 3; ++i)
             {
-                ans[i] = "g" + ans[i];
+                result[i].push_back('g');
             }
-            for (int i = ans.size() / 3; i < ans.size() / 3 * 2; ++i)
+            for (int i = result.size() / 3; i < result.size() / 3 * 2; ++i)
             {
-                ans[i] = "h" + ans[i];
+                result[i].push_back('h');
             }
-            for (int i = ans.size() / 3 * 2; i < ans.size(); ++i)
+            for (int i = result.size() / 3 * 2; i < result.size(); ++i)
             {
-                ans[i] = "i" + ans[i];
+                result[i].push_back('i');
             }
         }
         else if (digits[index] == '5')
         {
-            for (int i = 0; i < ans.size() / 3; ++i)
+            for (int i = 0; i < result.size() / 3; ++i)
             {
-                ans[i] = "j" + ans[i];
+                result[i].push_back('j');
             }
-            for (int i = ans.size() / 3; i < ans.size() / 3 * 2; ++i)
+            for (int i = result.size() / 3; i < result.size() / 3 * 2; ++i)
             {
-                ans[i] = "k" + ans[i];
+                result[i].push_back('k');
             }
-            for (int i = ans.size() / 3 * 2; i < ans.size(); ++i)
+            for (int i = result.size() / 3 * 2; i < result.size(); ++i)
             {
-                ans[i] = "l" + ans[i];
+                result[i].push_back('l');
             }
         }
         else if (digits[index] == '6')
         {
-            for (int i = 0; i < ans.size() / 3; ++i)
+            for (int i = 0; i < result.size() / 3; ++i)
             {
-                ans[i] = "m" + ans[i];
+                result[i].push_back('m');
             }
-            for (int i = ans.size() / 3; i < ans.size() / 3 * 2; ++i)
+            for (int i = result.size() / 3; i < result.size() / 3 * 2; ++i)
             {
-                ans[i] = "n" + ans[i];
+                result[i].push_back('n');
             }
-            for (int i = ans.size() / 3 * 2; i < ans.size(); ++i)
+            for (int i = result.size() / 3 * 2; i < result.size(); ++i)
             {
-                ans[i] = "o" + ans[i];
+                result[i].push_back('o');
             }
         }
         else if (digits[index] == '7')
         {
-            for (int i = 0; i < ans.size() / 4; ++i)
+            for (int i = 0; i < result.size() / 4; ++i)
             {
-                ans[i] = "p" + ans[i];
+                result[i].push_back('p');
             }
-            for (int i = ans.size() / 4; i < ans.size() / 2; ++i)
+            for (int i = result.size() / 4; i < result.size() / 2; ++i)
             {
-                ans[i] = "q" + ans[i];
+                result[i].push_back('q');
             }
-            for (int i = ans.size() / 2; i < ans.size() / 4 * 3; ++i)
+            for (int i = result.size() / 2; i < result.size() / 4 * 3; ++i)
             {
-                ans[i] = "r" + ans[i];
+                result[i].push_back('r');
             }
-            for (int i = ans.size() / 4 * 3; i < ans.size(); ++i)
+            for (int i = result.size() / 4 * 3; i < result.size(); ++i)
             {
-                ans[i] = "s" + ans[i];
+                result[i].push_back('s');
             }
         }
         else if (digits[index] == '8')
         {
-            for (int i = 0; i < ans.size() / 3; ++i)
+            for (int i = 0; i < result.size() / 3; ++i)
             {
-                ans[i] = "t" + ans[i];
+                result[i].push_back('t');
             }
-            for (int i = ans.size() / 3; i < ans.size() / 3 * 2; ++i)
+            for (int i = result.size() / 3; i < result.size() / 3 * 2; ++i)
             {
-                ans[i] = "u" + ans[i];
+                result[i].push_back('u');
             }
-            for (int i = ans.size() / 3 * 2; i < ans.size(); ++i)
+            for (int i = result.size() / 3 * 2; i < result.size(); ++i)
             {
-                ans[i] = "v" + ans[i];
+                result[i].push_back('v');
             }
         }
         else if (digits[index] == '9')
         {
-            for (int i = 0; i < ans.size() / 4; ++i)
+            for (int i = 0; i < result.size() / 4; ++i)
             {
-                ans[i] = "w" + ans[i];
+                result[i].push_back('w');
             }
-            for (int i = ans.size() / 4; i < ans.size() / 2; ++i)
+            for (int i = result.size() / 4; i < result.size() / 2; ++i)
             {
-                ans[i] = "x" + ans[i];
+                result[i].push_back('x');
             }
-            for (int i = ans.size() / 2; i < ans.size() / 4 * 3; ++i)
+            for (int i = result.size() / 2; i < result.size() / 4 * 3; ++i)
             {
-                ans[i] = "y" + ans[i];
+                result[i].push_back('y');
             }
-            for (int i = ans.size() / 4 * 3; i < ans.size(); ++i)
+            for (int i = result.size() / 4 * 3; i < result.size(); ++i)
             {
-                ans[i] = "z" + ans[i];
+                result[i].push_back('z');
             }
         }
 
-        return ans;
+        return result;
     }
 };
